b441849d07b231664498d8588b0033f036f45a35
Giba Pacce <gibapacce@gmail.com>
2021-07-26 17:18:47 -0300

    commit ousideclick bubble event

--- animaisfantasticos/js/script.js
+++ animaisfantasticos/js/script.js
@@ -3,9 +3,13 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initDropdownMenu();
