4905f4be57e52e287960b4496d2f1f5a518927e3
Giba Pacce <gibapacce@gmail.com>
2021-07-27 15:32:34 -0300

    commit final parte 5

--- animaisfantasticos/js/modules/scroll-animacao.js
+++ animaisfantasticos/js/modules/scroll-animacao.js
@@ -1,21 +1,21 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
-  if(sections.length) {
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else if (section.classList.contains("ativo")) {
+          section.classList.remove("ativo");
+        }
+      });
     }
 
     animaScroll();
 
-    window.addEventListener('scroll', animaScroll);
+    window.addEventListener("scroll", animaScroll);
   }
 }
--- animaisfantasticos/js/script.js
+++ animaisfantasticos/js/script.js
@@ -1,10 +1,13 @@
-import initScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +16,6 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
