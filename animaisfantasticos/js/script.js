877bc88e4e544471d76ee720b2780f18cee50ea4
Giba Pacce <gibapacce@gmail.com>
2021-08-06 10:40:18 -0300

    webpack instalado

--- animaisfantasticos/js/script.js
+++ animaisfantasticos/js/script.js
@@ -1,14 +1,14 @@
-import initScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
-import initFuncionamento from "./modules/funcionamento.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
 initAnimacaoScroll();
