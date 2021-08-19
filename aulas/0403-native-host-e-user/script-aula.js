b2b768857c3e991dfa9a763a2a9309402f41da0b
gibapacce <gibapacce@gmail.com>
2021-08-19 10:25:31 -0300

    Delete aulas directory

--- aulas/0403-native-host-e-user/script-aula.js
+++
@@ -1,8 +0,0 @@
-console.log(typeof Array.fromjdhsjkah !== "undefined")
-
-if(typeof Array.fromjdhsjkah !== "undefined") {
-  console.log('Existe')
-} else {
-  console.log('Não existe')
-}
-
--- aulas/0603-fetch/script-aula.js
+++
@@ -1,9 +0,0 @@
-const imagem = fetch('https://www.google.com');
-
-
-imagem.then(response => {
-  console.log(response.type);
-  if(response.status === 404) {
-    console.log('Página não existe');
-  }
-});
